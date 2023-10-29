64ce06999470c09b557b6aba7eaa1757d0555126
Bridget512 <bridget.wilson2@griffithuni.edu.au>
2023-10-29 14:54:06 +1000

    First Commit
    - Created a basic template for us to get started
    
    Please enter the commit message for your changes. Lines starting

---
+++ scripts/index.js
@@ -0,0 +1,15 @@
+
+
+function setup() {
+    var canvas = createCanvas(800, 600)
+    canvas.parent('p5playCanvas')
+    angleMode(DEGREES)
+    frameRate(60)
+
+}
+
+function draw() {
+    background(252)
+   
+
+}
